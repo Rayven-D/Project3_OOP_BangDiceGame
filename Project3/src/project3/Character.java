68ccff7e055654ceb8fe7d9c7b1182ccefc71cf9
Shreyesh <imshreyesh17@gmail.com>
2020-03-17 15:20:31 -0500

    character class init

---
+++ Project3/src/project3/Character.java
@@ -0,0 +1,31 @@
+//CS 2450 001 
+//Shreyesh Arangath 
+//shreyesh.arangath@ttu.edu
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project3;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shreyesh
+ */
+public class Character {
+    
+    public String name;
+    public int lifePoints;
+    public String specialAbility;
+    public static ArrayList<String> all;
+    
+    public Character(){
+        this.name = "";
+        this.lifePoints = 0;
+        this.specialAbility="";
+    }
+}
