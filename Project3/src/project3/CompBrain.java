afc8a335d73481162ab97da024b9451f637acd22
Jeffrey Tessitore <jeffptess@gmail.com>
2020-04-15 22:48:45 -0500

    Player + User/Comp Layout
    Layout for Comp/User, gets/sets for Player, and CompBrain interface

---
+++ Project3/src/project3/CompBrain.java
@@ -0,0 +1,17 @@
+/*
+// Jeffrey Tessitore
+// CS 2365 - Object-Oriented Programming
+// Section 002
+// Project 3
+ */
+package project3;
+
+/**
+ *
+ * @author parra
+ */
+public interface CompBrain {
+    boolean detAlly(int playerNum);
+    void findBestMove();
+    int findTarget();
+}
