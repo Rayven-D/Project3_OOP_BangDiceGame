7cc3a414f08580520b1cde2e206645fdeb7a77c7
Shreyesh <imshreyesh17@gmail.com>
2020-04-21 00:01:57 -0500

    UserPanel init

---
+++ Project3/src/controller/BoardViewController.java
@@ -0,0 +1,27 @@
+//CS 2450 001 
+//Shreyesh Arangath 
+//shreyesh.arangath@ttu.edu
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+
+/**
+ *
+ * @author shreyesh
+ */
+public class BoardViewController {
+    
+    public BoardViewController(){
+        
+    }
+    
+    
+    
+
+}
